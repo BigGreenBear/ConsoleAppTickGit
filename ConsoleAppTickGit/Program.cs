dba0df8872cfb1487edb11d10f73bfadc78d42d3
Michael Green <mikeg70uk@gmail.com>
2018-06-20 17:56:01 +0100

    Add project files.

---
+++ ConsoleAppTickGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTickGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
